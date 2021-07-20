c11f7213be65beb327188a485b41b7856fc046e2
Anthony Baryshnikov <abarankab@gmail.com>
2021-07-20 17:52:22 +0300

    Added noise schedules

--- ddpm/diffusion.py
+++ ddpm/diffusion.py
@@ -160,3 +160,25 @@ class GaussianDiffusion(nn.Module):
         
         t = torch.randint(0, self.num_timesteps, (b,), device=device)
         return self.get_losses(x, t, y)
+
+
+def generate_cosine_schedule(T, s=0.008):
+    def f(t, T):
+        return (np.cos((t / T + s) / (1 + s) * np.pi / 2)) ** 2
+    
+    alphas = []
+    f0 = f(0, T)
+
+    for t in range(T + 1):
+        alphas.append(f(t, T) / f0)
+    
+    betas = []
+
+    for t in range(1, T + 1):
+        betas.append(min(1 - alphas[t] / alphas[t - 1], 0.999))
+    
+    return np.array(betas)
+
+
+def generate_linear_schedule(T, low, high):
+    return np.linspace(low, high, T)
